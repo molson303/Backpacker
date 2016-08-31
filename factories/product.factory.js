426cee90a21c175667fd6cc3c8e76f812600f816
MIchael <michael.olson756@gmail.com>
2016-08-31 16:15:46 -0600

    shopping cart returning 0 when no products in cart

--- factories/product.factory.js
+++ factories/product.factory.js
@@ -8,23 +8,27 @@ app.factory('teaService', function () {
         product.quantity = quantity
         this.inventory.push(product);
       }
-      // first check if tea is in car already
-      // if it is, just update quantity
-      // else push tea in cart
+
 
     },
     getOrderTotal: function (){
+      zero = 0
       total = []
       for (var i = 0; i < this.inventory.length; i++) {
+
         var price = this.inventory[i].price;
         var quantity = this.inventory[i].quantity;
         var subTotal = Number(price * quantity)
         total.push(subTotal)
         }
+        if (total == 0) {
+          return zero
+        }else{
       var orderTotal = total.reduce(function (a, b) {
         return a+=b;
       })
       return orderTotal
+      }
     },
   }
 })
